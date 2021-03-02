8986bef2c39417ab43e392e75587bdb49625d4bc
marc <marc.lopezma.7e3@itb.cat>
2021-03-02 16:36:37 +0100

    First Layouts and Styles

--- GmailClone-app/src/androidTest/java/cat/itb/gmailclone/ExampleInstrumentedTest.java
+++ GmailClone-app/src/androidTest/java/cat/itb/gmailclone/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("cat.itb.twitterclone", appContext.getPackageName());
+        assertEquals("cat.itb.gmailclone", appContext.getPackageName());
     }
 }
