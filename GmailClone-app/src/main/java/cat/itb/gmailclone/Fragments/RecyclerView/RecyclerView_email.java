7d2495cd7986ec2c02c070ccba8f1a311f60f59a
Pau <pau>
2021-03-15 16:30:48 +0100

    Fixed NavDraw

--- GmailClone-app/src/main/java/cat/itb/gmailclone/Fragments/RecyclerView/RecyclerView_email.java
+++ GmailClone-app/src/main/java/cat/itb/gmailclone/Fragments/RecyclerView/RecyclerView_email.java
@@ -2,11 +2,16 @@ package cat.itb.gmailclone.Fragments.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,7 +25,7 @@ import static cat.itb.gmailclone.Model.EmailViewModel.emails;
 public class RecyclerView_email extends Fragment {
     RecyclerView recyclerView;
     EmailViewModel emailModel;
-
+    DrawerLayout drawer;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,6 +41,17 @@ public class RecyclerView_email extends Fragment {
         recyclerView = v.findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         EmailAdapter adapter = new EmailAdapter(emails);
+
+        //setSupportActionBar((Toolbar) v.findViewById(R.id.toolbar));
+
+//        drawer = v.findViewById(R.id.drawer);
+//
+//        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(getActivity(), drawer, R.string.open, R.string.close);
+//        drawer.addDrawerListener(drawerToggle);
+//        drawerToggle.syncState();
+
+      //  getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 //        adapter.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -50,4 +66,27 @@ public class RecyclerView_email extends Fragment {
         return v;
 
     }
+
+    // NO PARECE QUE ESTE METODO HAGA NADA (NAVIGATION DRAWER)
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawer.openDrawer(GravityCompat.START);
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+//    // NO PARECE QUE ESTE METODO HAGA NADA (NAVIGATION DRAWER)
+//    @Override
+//    public void onBackPressed() {
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            drawer.closeDrawer(GravityCompat.START);
+//            super.onBackPressed();
+//        }
+//    }
 }
--- GmailClone-app/src/main/java/cat/itb/gmailclone/MainActivity.java
+++ GmailClone-app/src/main/java/cat/itb/gmailclone/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,40 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        drawer = findViewById(R.id.drawer);
-
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close);
-        drawer.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
 
-    // NO PARECE QUE ESTE METODO HAGA NADA (NAVIGATION DRAWER)
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            drawer.openDrawer(GravityCompat.START);
-            return true;
-        }
-        else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
 
-    // NO PARECE QUE ESTE METODO HAGA NADA (NAVIGATION DRAWER)
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            drawer.closeDrawer(GravityCompat.START);
-            super.onBackPressed();
-        }
-    }
 
 }
